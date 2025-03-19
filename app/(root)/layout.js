50bf4eb739f301795fd227a5f0999be4138c477b
Duru C. Michael <durumikaelc@gmail.com>
2025-03-19 01:27:31 +0100

    remove redirect comment

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-const MainLayout = ({ children }) => {
-	// Redirect to onboarding
+const MainLayout = async ({ children }) => {
 	return <div className="container mx-auto mt-24 mb-20">{children}</div>;
 };
 
