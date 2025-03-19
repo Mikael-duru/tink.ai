98b6d9cf615d3da8c7fd6a1f538cbffcbd4d7421
Duru C. Michael <durumikaelc@gmail.com>
2025-03-19 01:24:33 +0100

    give users info to start each points on a new line

--- app/(root)/resume/_components/tool-tip.jsx
+++ app/(root)/resume/_components/tool-tip.jsx
@@ -10,16 +10,16 @@ import {
 const ToolTip = ({ type }) => {
 	const toolTipText = {
 		Summary: {
-			text: "Include your target job title, experience, achievements, and what makes you stand out—avoid vague phrases and end each line with a full stop",
+			text: "Include your target job title, years of experience, achievements, and what makes you stand out—avoid vague phrases.",
 		},
 		Skills: {
 			text: "List all your technical and soft skills in a single line, separated by commas.",
 		},
 		Experience: {
-			text: "Include key achievements, tools you used, and measurable impact. Focus on what you accomplished, not just what you did.",
+			text: "Include key achievements, tools you used, and measurable impact. Focus on what you accomplished, not just what you did. Start each point on a new line (use 'enter' key) and avoid vague descriptions.",
 		},
 		Project: {
-			text: "Describe your project's goal, technologies used, key features, and measurable impact—End each line with a full stop and avoid vague descriptions.",
+			text: "Describe your project's goal, technologies used, key features, and measurable impact—Start each point on a new line (use 'enter' key) and avoid vague descriptions.",
 		},
 	};
 	return (
